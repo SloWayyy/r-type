9c35f503f3e2d0e90910ff166e6d1cc6c144b134
kevin nadarajah <kevin.nadarajah@epitech.eu>
2023-12-04 13:25:54 +0100

    feat: folder client

---
+++ client/main.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** Rtype client
+** File description:
+** main.cpp
+*/
+
+#include "SFML/Graphics.hpp"
+
+int main(int ac, char **av)
+{
+    (void) ac;
+    (void) av;
+    return 0;
+}
