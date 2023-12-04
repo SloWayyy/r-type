ab1508ef5aef34d3919292c73cefa41035a8ed4d
kevin nadarajah <kevin.nadarajah@epitech.eu>
2023-12-04 13:26:30 +0100

    feat: folder server

---
+++ server/main.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** Rtype server
+** File description:
+** main.cpp
+*/
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    (void) av;
+    if (ac != 2) {
+        std::cerr << "USAGE: ./r-type_server port" << std::endl;
+        return 84;
+    }
+    return 0;
+}
