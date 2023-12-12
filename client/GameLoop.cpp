679824b7c464435a40f33cf59360c3b708334ad7
kevin nadarajah <kevin.nadarajah@epitech.eu>
2023-12-12 15:51:09 +0100

    fix(Compilation): Change vcpkg to fetch content to windows

--- client/GameLoop.cpp
+++ client/GameLoop.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "GameLoop.hpp"
+#include <SFML/Window.hpp>
 
 GameLoop::GameLoop(std::size_t TCPport, std::size_t UDPport, std::string ip)
     : _TCPclient(TCPport, ip),
@@ -30,6 +31,7 @@ GameLoop::~GameLoop()
 
 void GameLoop::run()
 {
+    sf::Window window(sf::VideoMode(800, 600), "R-Type");
     while (1) {
     }
 }
