1d7b588dd6aca1e67ca36e99c70fed6f4287de8b
Sloyi <rayan.es-seffar@epitech.eu>
2024-01-10 15:13:26 +0100

    fix: bad type for windows

--- server/system/ServerShootSystem.hpp
+++ server/system/ServerShootSystem.hpp
@@ -23,7 +23,7 @@ public:
         auto& position = _reg.getComponent<Position>();
         for (auto &tmp : _reg._eventManager.getEvent<shoot>()) {
             std::cout << "size " << _reg._eventManager.getEvent<shoot>().size() << std::endl;
-            u_int32_t id = _reg.addEntity();
+            uint32_t id = _reg.addEntity();
             bulletShot(id, position[tmp->entity_id].value().x, position[tmp->entity_id].value().y);
             _reg._eventManager.addEvent<bullet>(tmp->entity_id, id);
         }
