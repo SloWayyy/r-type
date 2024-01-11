a73be0131ca7b49b251da9e50fb7953b5ba94c95
kevin nadarajah <kevin.nadarajah@epitech.eu>
2024-01-11 23:24:48 +0100

    feat:(Server) add command heal and dead and split command

--- server/system/AdminCommandSystem.hpp
+++ server/system/AdminCommandSystem.hpp
@@ -7,23 +7,48 @@
 
 #ifndef ADMINCOMMANDSYSTEM_HPP_
 #define ADMINCOMMANDSYSTEM_HPP_
+#define HEALING "/heal"
+#define DEAD "/dead"
 
 #include "../../ecs/system/ISystem.hpp"
 #include "../../ecs/registry/registry.hpp"
 #include "../../network/tcpServer/tcpServer.hpp"
-#include <regex>
+
 
 class adminCommandSystem : public ISystem {
     public:
         adminCommandSystem() = delete;
         adminCommandSystem(registry &reg, TCPServer &tcpServer): _reg(reg), _tcpServer(tcpServer) {};
         ~adminCommandSystem() = default;
+        std::vector<std::string> split(const std::string &str, char delim) {
+            std::vector<std::string> result;
+            std::stringstream ss(str);
+            std::string item;
+
+            while (getline(ss, item, delim)) {
+                result.push_back(item);
+            }
+            return result;
+        }
+        void healing(std::vector<std::string> command) {
+            if (command.size() != 2)
+                return;
+            std::cout << "healing" << std::endl;
+        }
+        void dead(std::vector<std::string> command) {
+            if (command.size() != 2)
+                return;
+            std::cout << "dead" << std::endl;
+        }
         void operator()() override {
             _tcpServer._mtxQueueAdminCommand.lock();
             while (_tcpServer._queueAdminCommand.size() > 0) {
                 auto &tmp = _tcpServer._queueAdminCommand.back();
-                // first == port(tcp), second == command(msg)
+                std::vector<std::string> result = split(tmp.second, ' ');
                 std::cout << "admin command: " << tmp.second << std::endl;
+                if (_adminCommand.find(result[0]) != _adminCommand.end()) {
+                    _adminCommand.at(result[0])(result);
+                }
                 _tcpServer._queueAdminCommand.pop_back();
             }
             _tcpServer._mtxQueueAdminCommand.unlock();
@@ -31,6 +56,10 @@ class adminCommandSystem : public ISystem {
     private:
         registry &_reg;
         TCPServer &_tcpServer;
+        const std::unordered_map<std::string, std::function<void(std::vector<std::string>)>> _adminCommand = {
+            {HEALING, [this](std::vector<std::string> command) {healing(command);}},
+            {DEAD, [this](std::vector<std::string> command) {dead(command);}},
+        };
 };
 
 #endif /* !ANIMSYSTEM_HPP_ */
